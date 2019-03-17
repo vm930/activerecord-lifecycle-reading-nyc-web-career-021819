0d3f29733edbd83d3d9dfd2812ecc91e3356dc2b
Victoria Mei <vm930@hotmail.com>
2019-03-17 16:33:33 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only:[:new,:create,:update,:edit,:show,:index]
+  # resources :authors, only:[:]
 end
